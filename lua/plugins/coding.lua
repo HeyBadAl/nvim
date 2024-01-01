758b0a5537de8b41a507bdf70972ceb7c1bbe6f7
Badal <badalWork1@gmail.com>
2024-01-02 00:22:21 +0530

    feat: Update keymaps and add Comment.nvim plugin
    Refine keymaps in keymaps.lua for better navigation and introduce the Comment.nvim plugin in coding.lua to enhance commenting functionality.

--- lua/plugins/coding.lua
+++ lua/plugins/coding.lua
@@ -1,3 +1,12 @@
 return {
   { import = "plugins.extras.coding.codeium" },
+
+  -- comment
+  {
+    "numToStr/Comment.nvim",
+    opts = {
+      -- add any options here
+    },
+    lazy = false,
+  },
 }
