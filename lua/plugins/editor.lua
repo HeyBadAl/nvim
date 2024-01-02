e30f76179fe3f57819947308acd4fc1ad61ab058
Badal <badalWork1@gmail.com>
2024-01-02 16:34:00 +0530

    chore: Configure flash.nvim and update git plugins
    Disable flash.nvim and update the configuration for gitsigns.nvim. The configuration for gitsigns.nvim includes key mappings and adjustments for better Git integration and usability.

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -1,5 +1,20 @@
 local Util = require("lazyvim.util")
 return {
+  -- diable flash
+  {
+    enabled = false,
+    "folke/flash.nvim",
+    ---@type Flash.Config
+    opts = {
+      search = {
+        forward = true,
+        multi_window = false,
+        wrap = false,
+        incremental = true,
+      },
+    },
+  },
+
   { "nvim-neo-tree/neo-tree.nvim", enabled = false },
 
   -- nvim-tree
@@ -98,78 +113,4 @@ return {
       })
     end,
   },
-
-  -- gitsigns
-  {
-    "lewis6991/gitsigns.nvim",
-    event = "LazyFile",
-    opts = {
-      signs = {
-        add = { text = "▎" },
-        change = { text = "▎" },
-        delete = { text = "" },
-        topdelete = { text = "" },
-        changedelete = { text = "▎" },
-        untracked = { text = "▎" },
-      },
-      on_attach = function(buffer)
-        local gs = package.loaded.gitsigns
-
-        local function map(mode, l, r, desc)
-          vim.keymap.set(mode, l, r, { buffer = buffer, desc = desc })
-        end
-
-      -- stylua: ignore start
-      map("n", "]h", gs.next_hunk, "Next Hunk")
-      map("n", "[h", gs.prev_hunk, "Prev Hunk")
-      map({ "n", "v" }, "<leader>ghs", ":Gitsigns stage_hunk<CR>", "Stage Hunk")
-      map({ "n", "v" }, "<leader>ghr", ":Gitsigns reset_hunk<CR>", "Reset Hunk")
-      map("n", "<leader>ghS", gs.stage_buffer, "Stage Buffer")
-      map("n", "<leader>ghu", gs.undo_stage_hunk, "Undo Stage Hunk")
-      map("n", "<leader>ghR", gs.reset_buffer, "Reset Buffer")
-      map("n", "<leader>ghp", gs.preview_hunk, "Preview Hunk")
-      map("n", "<leader>ghb", function() gs.blame_line({ full = true }) end, "Blame Line")
-      map("n", "<leader>ghd", gs.diffthis, "Diff This")
-      map("n", "<leader>ghD", function() gs.diffthis("~") end, "Diff This ~")
-      map({ "o", "x" }, "ih", ":<C-U>Gitsigns select_hunk<CR>", "GitSigns Select Hunk")
-      end,
-    },
-  },
-  --
-  -- -- Telescope
-  -- {
-  --   "nvim-telescope/telescope.nvim",
-  --   config = function()
-  --     require("telescope").setup({
-  --       defaults = {
-  --         prompt_prefix = "󱞩 ",
-  --         selection_caret = " ",
-  --         path_display = { "smart" },
-  --         file_ignore_patterns = {
-  --           ".git",
-  --         },
-  --         -- -- include .github directory
-  --         -- file_include_patterns = {
-  --         -- 	".github/**",
-  --         -- 	".gitignore",
-  --         -- },
-  --       },
-  --       pickers = {
-  --         find_files = {
-  --           theme = "dropdown",
-  --           previewer = true,
-  --           hidden = true,
-  --         },
-  --         live_grep = {
-  --           theme = "dropdown",
-  --           previewer = true,
-  --         },
-  --         buffers = {
-  --           theme = "dropdown",
-  --           previewer = true,
-  --         },
-  --       },
-  --     })
-  --   end,
-  -- },
 }
